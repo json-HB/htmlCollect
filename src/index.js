3c9a4277029e08252abf394225256be7d320356b
jihaibo <17827855+json-HB@users.noreply.github.com>
2020-03-25 18:59:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-<script>
-if (document.domain == "json-hb.github.io") {
-  var base = document.createElement("base");
-  base.href = "/htmlCollect/";
-  base.target = "_self";
-  document.head.appendChild(base);
-}
-</script>
+console.log("hello");
