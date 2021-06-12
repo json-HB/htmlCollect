9ff7b59e6a326c3592eff7624870661cd0ba5bb6
jihaibo <17827855+json-HB@users.noreply.github.com>
2021-06-13 00:00:52 +0800

    update

--- src/util/sw.js
+++ src/util/sw.js
@@ -1,19 +1,24 @@
-self.addEventListener('install', (event) => {
-    console.log(event, 'event')
+self.addEventListener(
+  'install',
+  event => {
+    console.log(event, 'event');
     event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/img/pic0.jpg',
-                '/img/pic1.jpg',
-                '/img/pic2.jpg',
-            ])
-        })
-    )
-}, false)
+      caches.open('v1').then(cache => {
+        return cache.addAll(['/img/pic0.jpg', '/img/pic1.jpg', '/img/pic2.jpg']);
+      })
+    );
+  },
+  false
+);
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-      );
-      console.log(event)
-  });
+  console.log(event);
+  event.respondWidth(
+    caches.match(event.request).then(res => {
+      if (res === undefined) {
+        return new Response('hello');
+      }
+      return res;
+    })
+  );
+});
