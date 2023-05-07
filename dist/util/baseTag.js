af880743f8a5680b956ce26fde0bb9e7a1644389
jihaibo <17827855+json-HB@users.noreply.github.com>
2023-05-07 14:15:27 +0800

    qc

--- dist/util/baseTag.js
+++
@@ -1,6 +0,0 @@
-if (document.domain == 'json-hb.github.io') {
-  var base = document.createElement('base');
-  base.href = '/htmlCollect/';
-  base.target = '_self';
-  document.head.appendChild(base);
-}
